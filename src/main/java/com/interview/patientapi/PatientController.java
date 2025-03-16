141733017a8c8951d9d6b30969efa8308d146303
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2025-03-16 12:56:22 +0300

    implements pagination

--- src/main/java/com/interview/patientapi/PatientController.java
+++ src/main/java/com/interview/patientapi/PatientController.java
@@ -1,9 +1,11 @@
 package com.interview.patientapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -16,15 +18,34 @@ public class PatientController {
     @Autowired
     private PatientService patientService;
 
-    @GetMapping("/api/patients")
+    @GetMapping("/api")
     public ResponseEntity<?> getPatients() {
         List<PatientModel> patients = patientService.getPatients();
         Map<String, String> resp = new HashMap<>();
-        if (patients == null || patients.isEmpty()) {
+        if (patients == null) {
+            resp.put("message", "Failed to get patients");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resp);
+        }else if(patients.isEmpty()){
             resp.put("message", "No patients found");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(resp);
         }else{
             return ResponseEntity.status(HttpStatus.OK).body(patients);
         }
     }
+
+    @GetMapping("/api/patients")
+    public ResponseEntity<?> getPaginatedPatients(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size) {
+        Page<PatientModel> patients = patientService.getPaginatedPatients(page, size);
+
+        if (patients == null) {
+            Map<String, String> resp = new HashMap<>();
+            resp.put("message", "Failed to get patients");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resp);
+        }else {
+            return new ResponseEntity<>(patients, HttpStatus.OK);
+        }
+    }
+
 }
--- src/main/java/com/interview/patientapi/PatientService.java
+++ src/main/java/com/interview/patientapi/PatientService.java
@@ -1,8 +1,13 @@
 package com.interview.patientapi;
 
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.data.domain.Page;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -13,9 +18,28 @@ public class PatientService {
     public List<PatientModel> getPatients() {
         try{
             RestTemplate restTemplate = new RestTemplate();
-            PatientResponse response = restTemplate.getForObject(apiUrl, PatientResponse.class);
-            return response != null ? response.getUsers() : null;
+            PatientResponse patientResponse = restTemplate.getForObject(apiUrl, PatientResponse.class);
+            return patientResponse != null ? patientResponse.getUsers() : Collections.emptyList();
         }catch (Exception e){
+            return null;
+        }
+    }
+
+    public Page<PatientModel> getPaginatedPatients(int page, int size) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            PatientResponse patientResponse = restTemplate.getForObject(apiUrl, PatientResponse.class);
+            List<PatientModel> patients = (patientResponse != null && patientResponse.getUsers() != null)
+                    ? patientResponse.getUsers()
+                    : Collections.emptyList();
+
+            int start = Math.min(page * size, patients.size());
+            int end = Math.min((page + 1) * size, patients.size());
+
+            List<PatientModel> paginatedPatients = patients.subList(start, end);
+            Pageable pageable = PageRequest.of(page, size);
+            return new PageImpl<>(paginatedPatients, pageable, patients.size());
+        } catch (Exception e) {
             return null;
         }
     }
